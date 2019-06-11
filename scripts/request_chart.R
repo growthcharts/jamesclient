4ed2cbbd4508e50d43a92ff6f018f95d50ad37ad
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-06-11 20:39:13 +0200

    Update test script

--- scripts/request_chart.R
+++ scripts/request_chart.R
@@ -9,13 +9,8 @@ loc <- get_url(resp, "location")
 
 # create the chart
 
-resp2 <- req_chart(location = loc, chartcode = "WMAH")
+resp2 <- request_chart(ssd = resp, chartcode = "WMAH")
 browseURL(get_url(resp2, "svg"))
 
-resp2 <- req_chart(location = loc, chartcode = "PJAAN27")
+resp2 <- request_chart(ssd = resp, chartcode = "PJAAN27")
 browseURL(get_url(resp2, "svg", pad="?width=10&height=12"))
-
-# list the available charts
-# POST(paste0(resp$url
-
-# use function that uploads and process data
