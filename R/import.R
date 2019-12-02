b41fca15347f37a8f65bfdda153e2ac3fdac2468
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-12-02 15:03:18 +0100

    Add import

--- R/import.R
+++ R/import.R
@@ -3,5 +3,6 @@
 #'                          http_error http_type message_for_status
 #'                          modify_url parse_url POST status_code
 #'                          stop_for_status upload_file user_agent
+#'                          warn_for_status
 #' @importFrom jsonlite     fromJSON toJSON validate
 NULL
