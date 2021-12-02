2a581d39f8caa2a248d100af3fe1eb98ffacd449
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-12-02 14:28:02 +0100

    Restore example james_post()

--- R/james_post.R
+++ R/james_post.R
@@ -7,6 +7,7 @@
 #' encoding `multipart/form`. If `txt` is a URL, then the data are read from the URL
 #' as JSON string and uploaded with encoding `json`.
 #' @examples
+#' james_post()
 #' fn <- system.file("extdata", "allegrosultum", "client3.json", package = "jamesdemodata")
 #' url <- paste("http://localhost", "ocpu/library/jamesdemodata",
 #'   "extdata/bds_v2.0/smocc/Laura_S.json", sep = "/")
@@ -29,24 +30,29 @@ james_post <- function(path = c("version",
   url <- modify_url(url = host, path = path)
   ua <- user_agent("https://github.com/growthcharts/jamesclient")
   done <- FALSE
+  encoding <- "json"
 
   txt <- txt[1L]
-  # if we have a file name, request encode = "multipart"
-  if (file.exists(txt)) {
-    resp <- POST(url, ua,
-                 body = list(txt = upload_file(txt)),
-                 encode = "multipart")
-    done <- TRUE
+
+  if (!is.null(txt)) {
+    # if we have a file name, request encode = "multipart"
+    if (file.exists(txt)) {
+      encoding <- "multipart"
+      resp <- POST(url, ua,
+                   body = list(txt = upload_file(txt)),
+                   encode = "multipart")
+      done <- TRUE
+    }
   }
-  # if we have a URL, read as json string
-  if (url.exists(txt)) {
-    con <- curl(txt, open = "r")
-    txt <- readLines(con)
-    close(con)
-  }
-  # if we have valid JSON, request encode = "json"
   if (!done) {
-    if (validate(txt)) {
+    # if we have a URL, read as json string
+    if (!is.null(txt) && url.exists(txt)) {
+      con <- curl(txt, open = "r")
+      txt <- readLines(con)
+      close(con)
+    }
+    # if we have valid JSON, request encode = "json"
+    if (is.null(txt) || validate(txt)) {
       resp <- POST(url, ua,
                    body = list(txt = txt),
                    query = list(auto_unbox = TRUE),
