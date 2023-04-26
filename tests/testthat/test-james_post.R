b6d00731c5f9b1a891d90ea8ef136553026ccd54
Stef van Buuren <stef.vanbuuren@tno.nl>
2023-04-26 15:23:41 +0200

    Add test for data/upload/json

--- tests/testthat/test-james_post.R
+++ tests/testthat/test-james_post.R
@@ -1,6 +1,7 @@
 library(httr)
 
 host <- "http://localhost"
+# host <- "https://james.groeidiagrammen.nl"
 url <- file.path(host, "ocpu/library/bdsreader/examples/maria.json")
 
 if (valid_url(host) && valid_url(url)) {
@@ -29,5 +30,17 @@ if (valid_url(host) && valid_url(url)) {
                  chartcode = "PMAAN27", selector = "chartcode",
                  query = list(height = 29.7/2.54, width = 21/2.54))),
       201)
+  )
+}
+
+if (valid_url(host) && valid_url(url)) {
+  fn <- system.file("extdata/bds_v2.0/smocc/laura_s.json", package = "jamesdemodata")
+  # fn <- system.file("extdata/bds_v2.0/test/test1.json", package = "jamesdemodata")
+  data <- readLines(con = fn)
+  data2 <- read_json_js(fn)
+  resp <- james_post(host = host, path = "data/upload/json", txt = data)
+  test_that(
+    "Download demodata file",
+    expect_equal(status_code(resp), 201)
   )
 }
