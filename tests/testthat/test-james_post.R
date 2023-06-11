c79b54dcbab1863e903a1e89e1d18f995269cb49
Stef van Buuren <stef.vanbuuren@tno.nl>
2023-06-11 21:37:06 +0200

    Repair test error

--- tests/testthat/test-james_post.R
+++ tests/testthat/test-james_post.R
@@ -36,9 +36,8 @@ if (valid_url(host) && valid_url(url)) {
 if (valid_url(host) && valid_url(url)) {
   fn <- system.file("extdata/bds_v2.0/smocc/laura_s.json", package = "jamesdemodata")
   # fn <- system.file("extdata/bds_v2.0/test/test1.json", package = "jamesdemodata")
-  data <- readLines(con = fn)
-  data2 <- read_json_js(fn)
-  resp <- james_post(host = host, path = "data/upload/json", txt = data)
+  js <- read_json_js(fn)
+  resp <- james_post(host = host, path = "data/upload/json", txt = js)
   test_that(
     "Download demodata file",
     expect_equal(status_code(resp), 201)
